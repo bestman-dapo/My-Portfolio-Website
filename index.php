2cc9d9b009c298c79687d471aaa62930438c982b
bestman-dapo <oyebanjioladapo1@gmail.com>
2021-11-26 10:55:22 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <div class="indexContainer">
     <div class="intro">
         <h1 >I'm <br> <span>Oladapo Oyebanji</span>  <br> A Savy Web Developer</h1>
-        <p class="text"> a personal website is a good opportuinity to encompass the wide scope of your interest and work. This striking portfolio provides a peek into the world of Oladapo, with his keen eye of development, love for creativity, and a strong sense of styling. <br> Oladapo's website showcases his works, and offers high quality experience</p>
+        <p class="text">Making a personal website is a good opportuinity to encompass the wide scope of your interest and work. This striking portfolio provides a peek into the world of Oladapo, with his keen eye of development, love for creativity, and a strong sense of styling. <br> Oladapo's website showcases his works, and offers high quality experience</p>
         <div class="aboutLinks">
             <a href="https://www.instagram.com/bestman_dapo/" target="_blank" style="text-decoration: none;"><div class="iconwrapper1" ><i class="fab fa-instagram icon" ></i></div></a>
 
